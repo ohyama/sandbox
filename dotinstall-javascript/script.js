b2a1434481105d0fdf3024098c388a778fd4d697
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-09 06:15:43 +0900

    JavaScript入門 3 完了

--- dotinstall-javascript/script.js
+++ dotinstall-javascript/script.js
@@ -1,1 +1,18 @@
-console.log("hello world 2");
+/*
+	- 文字列
+	- 数値
+	- 真偽値 true / false
+	- オブジェクト
+	-- 配列
+	-- 関数
+	-- 組み込みオブジェクト
+	-- undefined
+	-- null
+ */
+
+var msg = "hello world",
+	x = 10,
+	y = 20;
+
+console.log(msg);
+console.log(x + y);
