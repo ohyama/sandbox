0249ad7d45da3c028cf4d550ed10c918f3cbc128
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-12 22:33:48 +0900

    JavaScript入門 23 完了

--- dotinstall-javascript/script.js
+++ dotinstall-javascript/script.js
@@ -1,35 +1,12 @@
-var obj = {
-	name : "ohyama",
-	email : "ohyama@gmail",
-	greet : function(name) {
-		console.log("hello " + name + " from " + this.email);
-	}
-};
+console.dir(window);
+console.log(window.outerHeight);
 
-console.log(obj["name"]);
-console.log(obj.name);
-console.log(obj.greet("ohyama"));
+var e = document.getElementById("msg");
+e.textContent = 'hello';
+e.style.color = 'red';
+e.className = 'myStyle';
 
+var g = document.createElement('p'),
+	t = document.createTextNode('hello world');
 
-
-var score = [100, 300, 500];
-console.log(score[0]);
-
-var m = [
-	[1, 2, 3],
-	[4, 5, 6]
-];
-console.log(m[1][1]);
-
-
-var i = 0;
-function show() {
-	console.log(i++);
-	var tid = setTimeout(function() {
-		show();
-	}, 1000);
-	if (tid > 10) {
-		clearTimeout(tid);
-	}
-}
-show();
+document.body.appendChild(g).appendChild(t);
