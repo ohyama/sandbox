e82839ef7ecb8483fbeb7666157938de50139341
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-14 06:49:31 +0900

    JQuery入門 6 完了

--- dotinstall-jquery/script.js
+++ dotinstall-jquery/script.js
@@ -1,15 +1,50 @@
 // $(document).ready(function() {
 
 $(function() {
+
+	/*
+	 * $('') セレクタ
+	 * html p h1 ul
+	 * id #main
+	 * class .item
+	 *
+	 */
+	
+	$('p').css('color', 'blue');
+
+	/* フィルタ
+	 * :eq()
+	 * :gt(), :lt()
+	 * :even, :odd
+	 * :contains()
+	 * :first, :last
+	 */
+
 	// $('#sub .item').css('color', 'blue');
 	// $('p, .item').css('color', 'blue');
 	// $('.item + .item').css('color', 'blue');
 
+	/*
+	 * > 直下の子要素
+	 *   それ以下の要素
+	 * , 複数の要素
+	 * + 隣接する要素
+	 */
+	
 	// $('#sub > li:eq(2)').css('color', 'red');
 	// $('#sub > li:gt(1)').css('color', 'red');
 	// $('#sub > li:even').css('color', 'red');
 	// $('#sub > li:contains(4)').css('color', 'red');
-	$('#sub > li:first').css('color', 'red');
+	// $('#sub > li:first').css('color', 'red');
+	
+	/* メソッドを使ったDOM指定
+	 * parent(), children()
+	 * next(), prev()
+	 * siblings() -兄弟要素
+	 */
+	// $('#sub').parent().css('color', 'red');
+	// $('#sub > :eq(2)').next().css('color', 'red');
+	$('#sub > :eq(2)').siblings().css('color', 'red');
 });
 
 /*
