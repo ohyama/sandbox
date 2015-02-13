593ad6ee8f2f75aa3502ce31b83cff7097f8312b
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-14 06:28:48 +0900

    JQuery入門 3 完了

--- dotinstall-jquery/script.js
+++ dotinstall-jquery/script.js
@@ -2,8 +2,15 @@
 
 $(function() {
 	$('p').css('color', 'red').hide('slow');
+
+	$('#main').css('color', 'blue');
+	$('.item').css('color', 'green');
 });
 
-
-
+/*
+ * $('') セレクタ
+ * html p h1 ul
+ * id #main
+ * class .item
+ */
 
