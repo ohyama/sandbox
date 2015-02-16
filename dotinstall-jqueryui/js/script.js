73f00871ab423e70743f77c6abc369f4f7aac172
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-17 06:35:02 +0900

    JQueryUI入門 5 完了

--- dotinstall-jqueryui/js/script.js
+++ dotinstall-jqueryui/js/script.js
@@ -3,7 +3,10 @@ $(function() {
 	$('#box').draggable({
 		axis: 'x',
 		opacity: 0.5,
-		handle: '.handle'
+		handle: '.handle',
+		drag: function(event, ui) {
+			console.log(ui.position);
+		}
 	});
 
 })
