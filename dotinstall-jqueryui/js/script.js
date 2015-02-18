ab5b6db70c551d0b978d302acdce5c6fa0bfadb4
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-19 08:13:06 +0900

    JQueryUI入門 10 完了

--- dotinstall-jqueryui/js/script.js
+++ dotinstall-jqueryui/js/script.js
@@ -1,4 +1,20 @@
 $(function() {
+	var selected = new Array();
+	$('#selectable').selectable({
+		selected: function(event, ui) {
+			if (selected.indexOf(ui.selected.id) == - 1)
+			{
+				selected.push(ui.selected.id);
+			}
+			console.log(selected);
+		},
+		unselected: function(event, ui) {
+			var id = ui.unselected.id;
+			selected.splice(selected.indexOf(id), 1);
+		}
+	
+	});
+
 	$('.box')
 		.resizable({
 			handles: 'all',
