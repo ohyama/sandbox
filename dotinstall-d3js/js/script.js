477a8421c647a191bcfc9622afaf12e8e626e176
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-22 21:52:54 +0900

    D3.js入門 14 完了

--- dotinstall-d3js/js/script.js
+++ dotinstall-d3js/js/script.js
@@ -5,10 +5,10 @@ var h = 200;
 
 var svg = d3.select('body').append('svg').attr({ width: w, height: h});
 
-svg.selectAll('circle')
+svg.selectAll('rect')
 	.data(dataset)
 	.enter()
-	.append('circle')
+	.append('rect')
 	.on('mouseover', function(d) {
 		d3.select(this).attr('fill', 'orange');
 	})
@@ -16,30 +16,14 @@ svg.selectAll('circle')
 		d3.select(this).attr('fill', 'red');
 	})
 	.on('click', function(d) {
-		var rs = d3.select(this).attr('r');
+		var rs = d3.select(this).attr('width');
 		alert(rs);
 	})
 	.attr({
-		cx: function (d, i) { return 50 + (i * 100); },
-		cy: h / 2,
-		r: function(d) { return d; },
+		x: 0,
+		y: function(d, i) { return i * 25 },
+		width: function(d) { return d; },
+		height: 20,
 		fill: 'red'
 	});
-
-var p = d3.select('body').selectAll('p');
-
-// data binding
-var update = p.data(dataset);
-var enter = update.enter();
-var exit = update.exit();
-
-update.text(function(d) {
-	return "update:" + d;
-});
-
-enter.append('p').text(function(d) {
-	return "enter:" + d;
-});
-
-exit.style('color', 'red');
 
