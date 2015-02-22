5c67f7cb78b78bf641a825ff71fc7d500bb5cecf
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-22 22:05:01 +0900

    D3.js入門 17 完了

--- dotinstall-d3js/js/script.js
+++ dotinstall-d3js/js/script.js
@@ -2,8 +2,25 @@ var dataset = [12, 14, 12, 34, 24];
 
 var w = 500;
 var h = 200;
+var padding = 20;
+
+var xScale = d3.scale.linear()
+				.domain([0, d3.max(dataset)])
+				.range([padding, w - padding])
+				.nice();
 
 var svg = d3.select('body').append('svg').attr({ width: w, height: h});
+
+var xAxis = d3.svg.axis()
+				.scale(xScale)
+				.orient('bottom');
+
+svg.append("g")
+	.attr({
+		class: 'axis',
+		transform: "translate(0, 180)"
+	})
+	.call(xAxis);
 
 svg.selectAll('rect')
 	.data(dataset)
@@ -20,9 +37,9 @@ svg.selectAll('rect')
 		alert(rs);
 	})
 	.attr({
-		x: 0,
+		x: padding,
 		y: function(d, i) { return i * 25 },
-		width: function(d) { return d; },
+		width: function(d) { return xScale(d); },
 		height: 20,
 		fill: 'red'
 	});
