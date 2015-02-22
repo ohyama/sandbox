90fd9564446a3e601ecbbccccd33d85fb84297ce
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-22 22:52:21 +0900

    Google Chart Tools入門 5 完了

--- dotinstall-google-chart-tools/js/script.js
+++ dotinstall-google-chart-tools/js/script.js
@@ -1,1 +1,27 @@
+google.load('visualization', '1.0', {packages : ['corechart']});
+google.setOnLoadCallback(drawChart);
 
+function drawChart() {
+	var data = new google.visualization.DataTable();
+	
+	data.addColumn('string', '活動');
+	data.addColumn('number', '時間');
+	
+	data.addRows([
+		['睡眠', 8],
+		['活動', 12],
+		['ネット', 4]
+	]);
+
+	var options = {
+		title: '1日の内訳',
+		width: 500,
+		height: 500,
+		pieSliceText: 'label', 
+		is3D: true
+	}
+
+	var chart = new google.visualization.PieChart(document.getElementById('chart'));
+
+	chart.draw(data, options);
+}
