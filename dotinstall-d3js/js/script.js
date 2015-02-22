e53db26e0e60d1114dad08067127648c0106e78f
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-22 20:18:15 +0900

    D3.js入門 10 完了

--- dotinstall-d3js/js/script.js
+++ dotinstall-d3js/js/script.js
@@ -1,4 +1,21 @@
-var dataset = [12, 24, 36, 48];
+var dataset = [12, 14, 12, 34, 24];
+
+var w = 500;
+var h = 200;
+
+var svg = d3.select('body').append('svg').attr({ width: w, height: h});
+
+svg.selectAll('circle')
+	.data(dataset)
+	.enter()
+	.append('circle')
+	.attr({
+		cx: function (d, i) { return 50 + (i * 100); },
+		cy: h / 2,
+		r: function(d) { return d },
+		fill: 'red'
+	});
+
 var p = d3.select('body').selectAll('p');
 
 // data binding
