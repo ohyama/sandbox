1ec6bd6d3fb997ba078519400ebcef1eb34750be
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-22 18:24:29 +0900

    D3.js入門 3 完了

---
+++ dotinstall-d3js/js/script.js
@@ -0,0 +1,17 @@
+/*
+ * 要素を指定
+ * select, selectAll
+ *
+ * 値を設定、取得
+ * text
+ * attr
+ * style
+ * append
+ * style
+ * append
+ * remove
+ */
+
+d3.select('body').selectAll('p').text('Hello D3.js');
+
+
