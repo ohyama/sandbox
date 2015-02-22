0ed65d8786e50d4f3929713a99fed22e064af1f1
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-22 18:37:55 +0900

    D3.js入門 4 完了

--- dotinstall-d3js/js/script.js
+++ dotinstall-d3js/js/script.js
@@ -12,6 +12,18 @@
  * remove
  */
 
-d3.select('body').selectAll('p').text('Hello D3.js');
+var p = d3.select('body').selectAll('p')
 
+p.text('Hello D3.js').style('font-size', function() {
+	return Math.floor(Math.random() * 20 + 10) + 'px'
+});
+
+/*
+p.text('Hello D3.js').style({
+	'font-size': '28px',
+	'font-weight': 'bold'
+});
+*/
+
+console.log(p.style('font-size'));
 
