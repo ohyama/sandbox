952f4e9743389bc73a5638ae513aceede276d2c7
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-27 04:15:54 +0900

    Node.js入門 13 完了

--- dotinstall-nodejs/server.js
+++ dotinstall-nodejs/server.js
@@ -1,20 +1,37 @@
 var http = require('http'),
 	fs = require('fs'),
-	ejs = require('ejs');
+	ejs = require('ejs')
+	qs = require('querystring');
 var settings = require('./settings');
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-var n = 0;
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
 var server = http.createServer();
-server.on('request', function(req, res) {
-	n++;
+
+var posts = [];
+
+function renderForm(posts, res) {
 	var data = ejs.render(template, {
-		title: 'Hello',
-		content: '<strong>world!</strong>',
-		n: n
+		posts: posts
 	});
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(data);
 	res.end();
+}
+
+server.on('request', function(req, res) {
+	
+	if (req.method === 'POST') {
+		req.data = "";
+		req.on('readable', function() {
+			req.data += req.read();
+		});
+		req.on('end', function () {
+			var query = qs.parse(req.data);
+			posts.push(query.name);
+			renderForm(posts, res);
+		});
+	} else {
+		renderForm(posts, res);
+	}
 });
 server.listen(settings.port, settings.host);
 console.log('Server linstening');
