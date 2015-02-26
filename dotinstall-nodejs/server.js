d90c4b8c2e1fb2938e46589d75accee0fdc44e61
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-26 20:13:37 +0900

    Node.js 入門 7 完了

--- dotinstall-nodejs/server.js
+++ dotinstall-nodejs/server.js
@@ -1,10 +1,22 @@
 var http = require('http');
 var settings = require('./settings');
 
+var msg ='';
 var server = http.createServer();
 server.on('request', function(req, res) {
+	switch (req.url) {
+		case '/about':
+			msg = 'about this page.';
+			break;
+		case '/profile':
+			msg = 'about me';
+			break;
+		default:
+			msg = 'wrong page';
+			break;
+	}
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('hello world');
+	res.write(msg);
 	res.end();
 });
 server.listen(settings.port, settings.host);
