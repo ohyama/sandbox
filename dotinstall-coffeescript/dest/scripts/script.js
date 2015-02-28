370215e42c68d8328311d6ed484f22b534fc0842
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-28 22:11:23 +0900

    CoffeeScript入門 13 完了

--- dotinstall-coffeescript/dest/scripts/script.js
+++ dotinstall-coffeescript/dest/scripts/script.js
@@ -1,75 +1,21 @@
 (function() {
-  var a, b, c, color, email, hello, i, j, len, msg, name, ref, ref1, results, score, sum, total, user;
+  var User, ohyama;
 
-  sum = 0;
-
-  total = (function() {
-    var j, results1;
-    results1 = [];
-    for (i = j = 0; j <= 9; i = j += 2) {
-      results1.push(sum += i);
+  User = (function() {
+    function User(name) {
+      this.name = name;
     }
-    return results1;
+
+    User.prototype.hello = function() {
+      return console.log("helle " + this.name);
+    };
+
+    return User;
+
   })();
 
-  console.log(total);
+  ohyama = new User("Ohyama");
 
-  ref = ["red", "blue", "pink"];
-  for (i = j = 0, len = ref.length; j < len; i = ++j) {
-    color = ref[i];
-    if (color !== "blue") {
-      console.log(color + ":" + i);
-    }
-  }
-
-  results = {
-    ohyama: 40,
-    nakayama: 30,
-    koyama: 50
-  };
-
-  for (name in results) {
-    score = results[name];
-    console.log(name + ":" + score);
-  }
-
-  hello = function(name) {
-    if (name == null) {
-      name = "ohyama";
-    }
-    return "hello " + name;
-  };
-
-  msg = hello("nakayama");
-
-  console.log(msg);
-
-  (function() {
-    return console.log("do!!!!");
-  })();
-
-  results = function(x) {
-    return [x, Math.pow(x, 2), Math.pow(x, 3)];
-  };
-
-  ref1 = results(5), a = ref1[0], b = ref1[1], c = ref1[2];
-
-  console.log(a);
-
-  console.log(b);
-
-  console.log(c);
-
-  user = {
-    name: "ohyama",
-    score: 50,
-    email: "hoge@hoge.com"
-  };
-
-  name = user.name, email = user.email;
-
-  console.log(name);
-
-  console.log(email);
+  ohyama.hello();
 
 }).call(this);
--- dotinstall-coffeescript/src/coffee/script.coffee
+++ dotinstall-coffeescript/src/coffee/script.coffee
@@ -1,36 +1,7 @@
-sum = 0
-total = (sum += i for i in [0..9] by 2)
-console.log total
+class User
+  constructor: (@name) ->
+  hello: -> console.log "helle #{@name}"
 
-console.log "#{color}:#{i}" for color, i in ["red", "blue", "pink"] when color isnt "blue"
+ohyama = new User "Ohyama"
+ohyama.hello()
 
-
-results =
-  ohyama: 40
-  nakayama: 30
-  koyama: 50
-
-console.log "#{name}:#{score}" for name, score of results
-
-
-hello = (name = "ohyama")->
-  "hello #{name}"
-msg = hello "nakayama"
-console.log msg
-
-do -> console.log "do!!!!"
-
-results = (x)-> [x, x ** 2, x ** 3]
-[a, b, c] = results 5
-console.log a
-console.log b
-console.log c
-
-user =
-  name: "ohyama"
-  score: 50
-  email: "hoge@hoge.com"
-
-{name, email} = user
-console.log name
-console.log email
