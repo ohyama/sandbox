7106118f5584e855eff3d790650b182b9a59b672
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-28 18:17:25 +0900

    gulp入門 7 完了

--- dotinstall-gulp/gulpfile.js
+++ dotinstall-gulp/gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('html', function() {
 	return gulp.src('./src/index.html')
 		.pipe(gulp.dest('./dest'));
 });
 
-gulp.task('msg', ['html'], function() {
-	console.log('hello');
+gulp.task('img', function() {
+	return gulp.src('./src/img/*.jpg')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dest/img'));
 });
 
-gulp.task('default', ['msg']);
+gulp.task('default', ['html', 'img']);
