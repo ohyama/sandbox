aca9a73c3d408f2a9b3693cc29f6e324b51c492c
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-28 14:13:15 +0900

    Grunt入門 9 完了

--- dotinstall-grunt/Gruntfile.js
+++ dotinstall-grunt/Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 
 	// config
 	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
 		less: {
 			build1: {
 				src: 'src/style1.less',
@@ -12,18 +15,39 @@ module.exports = function(grunt) {
 					'build/styles2.css' : 'src/style2.less'
 				},
 				options: {
-					compress: true
+					compress: false
 				}
 
 			}
-		}
+		},
+
+		csslint: {
+			check: {
+				src: '<%= less.build1.dest %>'
+			}
+		},
+
+		cssmin: {
+			minimize: {
+				options: {
+					banner: '/*! <%= pkg.name  %> <%= grunt.template.today("yyyy/mm/dd") %> */'
+				}, 
+				files: {
+					'build/style1.min.css': '<%= less.build1.dest %>'
+				}
+			}
+		},
+
 	});
 
 	// pulgin
 	grunt.loadNpmTasks('grunt-contrib-less');
-	
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
 	// tasks
-	grunt.registerTask('default', 'less');
+	grunt.registerTask('default', ['less', 'csslint', 'cssmin']);
 	grunt.registerTask('task1', 'less:build1');
 	grunt.registerTask('task2', 'less:build2');
 };
