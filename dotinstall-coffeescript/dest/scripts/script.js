3b96af93aefbd0492af8a15c1cd27a8e61ed901a
Ohyama_ <yuki.ohyama+github@gmail.com>
2015-02-28 22:02:57 +0900

    CoffeeScript入門 11 完了

--- dotinstall-coffeescript/dest/scripts/script.js
+++ dotinstall-coffeescript/dest/scripts/script.js
@@ -1,74 +1,51 @@
 (function() {
-  var a, b, l, m, n, score, signal;
+  var color, hello, i, j, len, msg, name, ref, results, score, sum, total;
 
-  signal = "green";
+  sum = 0;
 
-  switch (signal) {
-    case "red":
-      console.long("STOP!");
-      break;
-    case "bule":
-    case "green":
-      console.log("GO!");
-      break;
-    case "yellow":
-      console.log("CAUTION");
-      break;
-    default:
-      console.log("Wrong Signal");
-  }
-
-  score = 82;
-
-  if (score > 80) {
-    console.log("score: " + (score * 10));
-  }
-
-  if (score > 60) {
-    console.log(socre);
-  }
-
-  if (score > 60) {
-    console.log(score);
-  } else {
-    console.log("ng");
-  }
-
-  a = [1, 3, 5];
-
-  b = [1, 2, 3];
-
-  m = [0, 1, 2, 3, 4, 5];
-
-  n = [0, 1, 2, 3, 4];
-
-  console.log(m.slice(1, 4));
-
-  console.log("world".slice(1, 4));
-
-  m = {
-    name: "ohyama",
-    score: 80
-  };
-
-  n = {
-    name: "ohyama",
-    score: 90
-  };
-
-  l = {
-    name: "ohyama",
-    score: {
-      a: 10,
-      b: 10,
-      c: 20
+  total = (function() {
+    var j, results1;
+    results1 = [];
+    for (i = j = 0; j <= 9; i = j += 2) {
+      results1.push(sum += i);
     }
+    return results1;
+  })();
+
+  console.log(total);
+
+  ref = ["red", "blue", "pink"];
+  for (i = j = 0, len = ref.length; j < len; i = ++j) {
+    color = ref[i];
+    if (color !== "blue") {
+      console.log(color + ":" + i);
+    }
+  }
+
+  results = {
+    ohyama: 40,
+    nakayama: 30,
+    koyama: 50
   };
 
-  console.log(m);
+  for (name in results) {
+    score = results[name];
+    console.log(name + ":" + score);
+  }
 
-  console.log(n);
+  hello = function(name) {
+    if (name == null) {
+      name = "ohyama";
+    }
+    return "hello " + name;
+  };
 
-  console.log(l);
+  msg = hello("nakayama");
+
+  console.log(msg);
+
+  (function() {
+    return console.log("do!!!!");
+  })();
 
 }).call(this);
--- dotinstall-coffeescript/src/coffee/script.coffee
+++ dotinstall-coffeescript/src/coffee/script.coffee
@@ -1,47 +1,21 @@
-signal = "green"
+sum = 0
+total = (sum += i for i in [0..9] by 2)
+console.log total
 
-switch signal
-  when "red"
-    console.long "STOP!"
-  when "bule", "green" then console.log "GO!"
-  when "yellow" then console.log "CAUTION"
-  else console.log "Wrong Signal"
+console.log "#{color}:#{i}" for color, i in ["red", "blue", "pink"] when color isnt "blue"
 
 
-score = 82
-if score > 80 then console.log "score: #{score * 10}"
+results =
+  ohyama: 40
+  nakayama: 30
+  koyama: 50
 
-console.log socre if score > 60
+console.log "#{name}:#{score}" for name, score of results
 
-if score > 60
-  console.log score
-else
-  console.log "ng"
 
-a = [1, 3, 5]
-b = [
-  1
-  2
-  3
-]
+hello = (name = "ohyama")->
+  "hello #{name}"
+msg = hello "nakayama"
+console.log msg
 
-m = [0..5]
-n = [0...5]
-
-console.log m[1..3]
-console.log "world"[1..3]
-
-m = name: "ohyama", score: 80
-n =
-  name: "ohyama"
-  score: 90
-l =
-  name: "ohyama"
-  score:
-    a: 10
-    b: 10
-    c: 20
-
-console.log m
-console.log n
-console.log l
+do -> console.log "do!!!!"
