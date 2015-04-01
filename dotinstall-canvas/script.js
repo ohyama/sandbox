589016bca5ec35eab60bbce1fe22b0a8f69b2bcc
Ohyama_ <yuki.ohyama@gmail.com>
2015-04-02 06:49:06 +0900

    dotinstall canvas #4

--- dotinstall-canvas/script.js
+++ dotinstall-canvas/script.js
@@ -1,8 +1,11 @@
 $(function(){
 
-	var canvas = document.getElementById('myCanvas');
+	var canvas = $('#myCanvas')[0];
+	//var canvas = document.getElementById('myCanvas');
 	if (!canvas || !canvas.getContext) return false;
 	
-	var ctx = canvas.getContext('2s');
-	ctx.strokeRect(10, 10, 50, 50);
+	var ctx = canvas.getContext('2d');
+	ctx.strokeRect(10, 10, 150, 50);
+	ctx.fillRect(10, 10, 100, 100);
+	ctx.clearRect(10, 10, 50, 50);
 });
