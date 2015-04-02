c2ce39b7aee4e6c39fadbd1b5794978c8b3a7a00
Ohyama_ <yuki.ohyama@gmail.com>
2015-04-02 23:35:54 +0900

    dotinstall canvas #5

--- dotinstall-canvas/script.js
+++ dotinstall-canvas/script.js
@@ -5,7 +5,15 @@ $(function(){
 	if (!canvas || !canvas.getContext) return false;
 	
 	var ctx = canvas.getContext('2d');
+
+	ctx.strokeStyle = "rgb(255, 0, 0)";
+	ctx.lineWidth = 10;
+	//ctx.lineJoin = "round";
+	//ctx.lineJoin = "bevel";
+	ctx.lineJoin = "miter";
 	ctx.strokeRect(10, 10, 150, 50);
+
+	ctx.fillStyle = "rgb(255, 0, 0)";
 	ctx.fillRect(10, 10, 100, 100);
 	ctx.clearRect(10, 10, 50, 50);
 });
