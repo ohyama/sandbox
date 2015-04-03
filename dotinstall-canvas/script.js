7d6d9cba861c7f7b2acd801be35515f148499bf6
Ohyama_ <yuki.ohyama@gmail.com>
2015-04-03 09:15:07 +0900

    dotinstall canvas #6

--- dotinstall-canvas/script.js
+++ dotinstall-canvas/script.js
@@ -14,6 +14,12 @@ $(function(){
 	ctx.strokeRect(10, 10, 150, 50);
 
 	ctx.fillStyle = "rgb(255, 0, 0)";
+	//var g = ctx.createLinearGradient(0,0,100,100);
+	var g = ctx.createRadialGradient(50,50,30,50,50,60);
+	g.addColorStop(0.0, "red");
+	g.addColorStop(0.5, "yellow");
+	g.addColorStop(1.0, "blue");
+	ctx.fillStyle = g;
 	ctx.fillRect(10, 10, 100, 100);
 	ctx.clearRect(10, 10, 50, 50);
 });
