451bbbad7565833c4da2de7f21c8da1b478ee982
Ohyama_ <yuki.ohyama@gmail.com>
2015-04-13 17:46:24 +0900

    dotinstall canvas #17

--- dotinstall-canvas/script.js
+++ dotinstall-canvas/script.js
@@ -7,6 +7,17 @@ $(function(){
 	var ctx = canvas.getContext('2d');
 
 	ctx.save();
+	
+	ctx.fillStyle = "red";
+	var y = 0;
+
+	(function loop() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		if (y > canvas.height) y = -50;
+		y++;
+		ctx.fillRect(0, y, 50, 50);
+		setTimeout(loop, 10);
+	})();
 
 	ctx.globalAlpha = 0.5;
 
