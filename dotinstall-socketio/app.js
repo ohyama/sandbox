e495d5c681a936814378a13c46cd17294daa1601
Ohyama_ <yuki.ohyama@gmail.com>
2015-04-20 19:42:23 +0900

    dotinstall socket.io #10

--- dotinstall-socketio/app.js
+++ dotinstall-socketio/app.js
@@ -18,10 +18,14 @@ function handler(req, res) {
 	});
 }
 
-io.sockets.on('connection', function(socket) {
+var chat = io.of('/chat').on('connection', function(socket) {
 	socket.on('emit_from_client', function(data) {
 		socket.join(data.room);
 		socket.emit('emit_from_server', 'you are in ' + data.room);
 		socket.broadcast.to(data.room).emit('emit_from_server', data.msg);
 	});
+});
+
+var news = io.of('/news').on('connection', function(socket) {
+	socket.emit('emit_from_server', 'today: ' + new Date());
 });
