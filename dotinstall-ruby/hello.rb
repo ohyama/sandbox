1cd1523ac153c1c8bfa91ddafe6d5abebef6340b
Ohyama_ <yuki.ohyama@gmail.com>
2015-05-11 06:05:48 +0900

    dotinstall ruby #18

--- dotinstall-ruby/hello.rb
+++ dotinstall-ruby/hello.rb
@@ -1,16 +1,6 @@
-=begin
-for i in 0..2 do
-	puts i
+def sayHi(name = "Steve")
+	puts "hello!" + name
 end
-for color in ["red", "green", "pink"] do
-	puts color
-end
-["red", "blue", "pink"].each do |color|
-	puts color
-end
-=end
 
-{"red" => 200, "blue" => 100, "pink" => 50}.each do |color, price|
-	puts "#{color}: #{price}"
-end
+sayHi()
 
