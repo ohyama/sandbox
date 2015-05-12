f5f32fb2ab29e2ed96fedfd1cea1fcfe28ae450f
Ohyama_ <yuki.ohyama@gmail.com>
2015-05-12 23:51:55 +0900

    dotinstall ruby #19

--- dotinstall-ruby/hello.rb
+++ dotinstall-ruby/hello.rb
@@ -1,6 +1,18 @@
-def sayHi(name = "Steve")
-	puts "hello!" + name
+class User 
+
+	def initialize(name)
+		@name = name
+	end
+
+	def sayHi
+		puts "hello, my name is #{@name}"
+	end
 end
 
-sayHi()
+tom = User.new("Tom")
+bob = User.new("Bob")
+
+tom.sayHi()
+bob.sayHi()
+
 
