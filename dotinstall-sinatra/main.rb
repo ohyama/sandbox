7fd72c4eb8306a5f7e9a3d039b162dffa7d6ef4f
Ohyama_ <yuki.ohyama@gmail.com>
2015-06-25 07:34:03 +0900

    dotinstall sinatra #12

--- dotinstall-sinatra/main.rb
+++ dotinstall-sinatra/main.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.estableish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
+end
 
 get '/' do
-
+  @comments = Comment.order("id desc").all
+  erb :index
 end
 
